708daf8b0ad0e525b1a33b177fcbc0ed2ac620d6
Sergey1996n <68687480+Sergey1996n@users.noreply.github.com>
2021-10-11 01:37:36 +0300

    Master

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnReview = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnReview);
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbPath);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(315, 199);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Загрузка изображения";
+            // 
+            // btnReview
+            // 
+            this.btnReview.Location = new System.Drawing.Point(207, 71);
+            this.btnReview.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReview.Name = "btnReview";
+            this.btnReview.Size = new System.Drawing.Size(100, 28);
+            this.btnReview.TabIndex = 4;
+            this.btnReview.Text = "Обзор";
+            this.btnReview.UseVisualStyleBackColor = true;
+            this.btnReview.Click += new System.EventHandler(this.btnReview_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(207, 159);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 28);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Посчитать";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Путь";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(8, 39);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(297, 22);
+            this.tbPath.TabIndex = 2;
+            this.tbPath.Text = "Шестеренки.png";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "jpg|*.jpg|png|*.png";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth4Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(239, 142);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(373, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(432, 639);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 639);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnReview;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        Image<Gray, byte> inputImage = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReview_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult result = openFileDialog1.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    inputImage = new Image<Gray, byte>(openFileDialog1.FileName);
+                    tbPath.Text = openFileDialog1.FileName;
+                    btnCalculate_Click(this, null);
+                }
+                else
+                    MessageBox.Show("Файл не выбран", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            /* Преобразуем картинку для бинаризации цветов */
+            inputImage = new Image<Gray, byte>(tbPath.Text);
+            for (int x = 0; x < inputImage.Cols; x++)
+            {
+                for (int y = 0; y < inputImage.Rows; y++)
+                {
+                    if (inputImage[y, x].Intensity < 150)
+                        inputImage[y, x] = new Gray(0);
+                    else
+                        inputImage[y, x] = new Gray(255);
+
+                }
+            }
+
+            imageList1.Images.Add(inputImage.Bitmap);
+            listView1.Items.Add("Исходное изображение", 0);
+
+            /* Создаём матрицу для нахождения середин отверстий*/
+            Matrix<byte> hole_ring = new Matrix<byte>(92, 92);
+            for (int col = 0; col <= hole_ring.Cols * 2 / 3; col++)
+            {
+                hole_ring[0, hole_ring.Cols / 6 + col] = 1;
+                hole_ring[hole_ring.Rows - 1, hole_ring.Cols / 6 + col] = 1;
+                hole_ring[hole_ring.Rows / 6 + col, 0] = 1;
+                hole_ring[hole_ring.Rows / 6 + col, hole_ring.Cols - 1] = 1;
+            }
+            for (int i = 0; i < hole_ring.Cols / 6 - 1; i++)
+            {
+                hole_ring[hole_ring.Cols / 6 - 1 - i, 1 + i] = 1;
+                hole_ring[hole_ring.Cols / 6 - 1 - i, hole_ring.Cols - 2 - i] = 1;
+                hole_ring[hole_ring.Cols - hole_ring.Cols / 6 + i, 1 + i] = 1;
+                hole_ring[hole_ring.Cols - hole_ring.Cols / 6 + i, hole_ring.Cols - 2 - i] = 1;
+            }
+
+            /* Находим середины отверстий */
+            #region MorphologyEx
+            /* operation - оператор морфологической математики */
+            /* kernel - элемент обхода */
+            /* anchor - смещение середины */
+            /* iterations - колическтво итераций */
+            /* borderType - тип границы */
+            /* borderValue - значение границы */
+            #endregion
+            Image<Gray, byte> outImage = inputImage.MorphologyEx(MorphOp.HitMiss, hole_ring, new Point(-1, -1), 1,
+                BorderType.Default, new MCvScalar());
+
+            imageList1.Images.Add(outImage.Bitmap);
+            listView1.Items.Add("Находим середины отверстий", 1);
+
+            /* Увеличиваем точки середин отверстий и соединяем с первоначальной картинкой */
+            #region GetStructuringElement
+            /* shape - форма элемента */
+            /* ksize - размер элемента */
+            /* anchor - смещение середины */
+            #endregion
+            Mat hole_mask = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(9, 9), new Point(-1, -1));
+            outImage = outImage.MorphologyEx(MorphOp.Dilate, hole_mask, new Point(-1, -1), 12,
+                BorderType.Default, new MCvScalar());
+            imageList1.Images.Add(outImage.Bitmap);
+            listView1.Items.Add("Увеличиваем середины отверстий", 2);
+
+            outImage = outImage.Or(inputImage);
+            imageList1.Images.Add(outImage.Bitmap);
+            listView1.Items.Add("Заполняем середины отверстий", 3);
+
+            /* Отделяем зубцы от дисков */
+            Mat gear_body = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(285, 285), new Point(-1, -1));
+            outImage = outImage.MorphologyEx(MorphOp.Open, gear_body, new Point(-1, -1), 1,
+                BorderType.Default, new MCvScalar());
+            imageList1.Images.Add(outImage.Bitmap);
+            listView1.Items.Add("Отделяем зубцы от дисков", 4);
+
+            /* Увеличиваем диски*/
+            Mat sampling_ring_spacer = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(9, 9), new Point(-1, -1));
+            outImage = outImage.MorphologyEx(MorphOp.Dilate, sampling_ring_spacer, new Point(-1, -1), 1,
+                BorderType.Default, new MCvScalar());
+            imageList1.Images.Add(outImage.Bitmap);
+            listView1.Items.Add("Увеличиваем диски", 5);
+
+            /* Ещё увеличиваем диски и отнимаем предыдущий вариант */
+            Mat sampling_ring_width = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(9, 9), new Point(-1, -1));
+            var tempImage = outImage.MorphologyEx(MorphOp.Dilate, sampling_ring_width, new Point(-1, -1), 2,
+                BorderType.Default, new MCvScalar());
+            tempImage -= outImage;
+            imageList1.Images.Add(tempImage.Bitmap);
+            listView1.Items.Add("Получаем кольцо на местах зубьев", 6);
+
+            /* Оставляем только зубцы */
+            outImage = tempImage.And(inputImage);
+            imageList1.Images.Add(outImage.Bitmap);
+            listView1.Items.Add("Оставляем только зубцы", 7);
+
+            /* Наращиваем зубцы */
+            Mat tip_spacing = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(9, 9), new Point(-1, -1));
+            outImage = outImage.MorphologyEx(MorphOp.Dilate, tip_spacing, new Point(-1, -1), 2,
+                BorderType.Default, new MCvScalar());
+            imageList1.Images.Add(outImage.Bitmap);
+            listView1.Items.Add("Наращиваем зубцы", 8);
+
+            /* Оставляем области с отсутствующими зубцами */
+            tempImage -= outImage;
+            imageList1.Images.Add(tempImage.Bitmap);
+            listView1.Items.Add("Оставляем области с отсутствующими зубцами", 9);
+
+            /* Увеличиваем области с отсутствующими зубцами, чтобы их было видно */
+            Mat defect_cue = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(37, 37), new Point(-1, -1));
+            tempImage = tempImage.MorphologyEx(MorphOp.Dilate, defect_cue, new Point(-1, -1), 1,
+                BorderType.Default, new MCvScalar());
+            imageList1.Images.Add(tempImage.Bitmap);
+            listView1.Items.Add("Увеличиваем области с отсутствующими зубцами", 10);
+
+            /* Получаем нужную картинку */
+            outImage = tempImage.Or(outImage);
+            imageList1.Images.Add(outImage.Bitmap);
+            listView1.Items.Add("Полученное изображение", 11);
+
+            //pictureBox2.Image = outImage.Bitmap;
+        }
+    }
+}
